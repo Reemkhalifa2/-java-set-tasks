a8d097facb3c39b4ff129fb10803f9c47a8c91ce
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-13 21:37:48 +0400

    add task 11

---
+++ src/BookSetDemo.java
@@ -0,0 +1,29 @@
+import java.util.*;        // Step 2: Import Set interface
+public class BookSetDemo {
+    public static void main(String[] args) {
+        Set<String> bookTitles = new HashSet<>();
+        bookTitles.add("The Silent Patient");
+        bookTitles.add("Project Hail Mary");
+        bookTitles.add("It Ends with Us");
+        bookTitles.add("The Correspondent");
+        bookTitles.add("Jane Eyre");
+        System.out.println(bookTitles);
+        bookTitles.add("Jane Eyre");
+
+        System.out.println(bookTitles);
+
+        System.out.println(bookTitles.remove("Jane Eyre"));
+
+        bookTitles.remove("Data Structures");
+
+        System.out.println(bookTitles);
+
+        System.out.println(bookTitles.size());
+
+        bookTitles.clear();
+
+        System.out.println(bookTitles);
+
+        System.out.println(bookTitles.size());
+    }
+}
