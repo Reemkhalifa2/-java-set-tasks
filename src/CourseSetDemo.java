48ae211fe8c96341ba7313ac24fb00cfa8b47647
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-14 09:59:42 +0400

    update task 10

--- src/CourseSetDemo.java
+++ src/CourseSetDemo.java
@@ -1,38 +1,31 @@
-import java.util.*;
+import java.util.*;        // Step 2: Import Set interface
 public class CourseSetDemo {
     public static void main(String[] args) {
         Set<String> courses = new HashSet<>();
         courses.add("Java Programming");
         courses.add("Web Development");
-        courses.add("Database");
-        courses.add("Web Design");
+        courses.add("Data Structures");
+        courses.add("Database Systems");
         courses.add("Python");
-        // Print all elements in the Set
         System.out.println(courses);
-        courses.add("Networking");
-        //add duplicate element
-        courses.add("Python");
-        //the duplicate value is not stored again because a Set
-        //allows only unique elements
-        System.out.println(courses);
+        courses.add("Java Programming");
+        //Print the element in the set
+        for (String x : courses) {
+            System.out.println(courses);
+        }
 
-        System.out.println(courses.contains("Python"));// check for another course name that does not exist in the set
-        System.out.println(courses.contains("Data Analysis"));// check for another course name that does not exist in the set
+        System.out.println(courses.contains("Web Development"));
 
-        //Use the remove() method to delete one existing element
-        courses.remove("Database");
-        System.out.println(courses);
-        courses.remove("Python");
-        //Display the size of the set
-        System.out.println(courses.size());
-        courses.add("Compute Architecture");
+        courses.remove("Data Structures");
 
         System.out.println(courses);
+
         System.out.println(courses.size());
 
         courses.clear();
+
         System.out.println(courses);
 
-        System.out.println("the set now contains zero elements "+courses.size());
+        System.out.println(courses.size());
     }
 }
